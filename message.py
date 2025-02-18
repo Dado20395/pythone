b21b22ec1152dd690b7817bb3aab4480da9b0063
Dado20395 <dado20395@icloud.com>
2025-02-18 11:30:40 +0100

    Create message.py

---
+++ message.py
@@ -0,0 +1,16 @@
+class Message :
+    def __init__(self, mittente, destinatario):
+        self.mittente = mittente
+        self.destinatario = destinatario
+        self.testo = " "
+    def append(self, riga):
+        self.testo += riga + "\n"
+    def toString(self):
+        return "Mittente: "  + self.mittente + "\nDestinatario: " + self.destinatario + "\n" + self.testo
+
+messaggio = Message("mittente@example.com", "destinatario@example.com")
+messaggio.append("Ciao, come stai?")
+messaggio.append("Spero che tu stia passando una buona giornata.")
+messaggio.append("A presto!")
+
+print(messaggio.toString())
